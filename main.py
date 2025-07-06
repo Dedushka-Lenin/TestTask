a435ed1034bb2d9a422a1599deb276e3e02402d1
Dedushka-Lenin <150888199+Dedushka-Lenin@users.noreply.github.com>
2025-07-06 22:51:59 +0300

    Preview version without database
    all endpoints are made except for endpoints for working with user's friends

--- main.py
+++ main.py
@@ -2,41 +2,65 @@ import uvicorn
 import json
 
 from typing import Literal, List, Optional
-from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr, constr
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse, JSONResponse
+from pydantic import BaseModel, EmailStr, constr, Field
 
 
 
-#  Разделить name на ФИО
-#  Сделать endpoint, который будет принимать фамилию человека и выводить по нему всю сводную информацию.
-#  Сделать endpoint, который изменяет информацию о пользователе.
-#  Сделать БД
 #  Создать возможность связывать людей в дружеские отношения. Дать возможность вывести всех друзей пользователя.
-#  endpoint, который выводит список всех пользователей --- список ФИО
-
+#  Сделать БД
 
 
 
 ####################################################################################################
 
-with open('data.json', 'r') as f:
-    data = json.load(f)
+try:
+    with open('data.json', 'r') as f:
+        data = json.load(f)
 
-# print(data)
+except:
+    data = {}
 
 class People(BaseModel):
-    id: constr(pattern=r'^\d{16}$')
-    name: str
-    age: int
+    # id: constr(pattern=r'^\d{16}$')
+    surname: str = Field(max_length=20)
+    name: str = Field(max_length=20)
+    patronymic: str = Field(max_length=20)
+    age: int = Field(ge=0, le=150)
     gender: Literal['m', 'w']
-    nationality: str
+    nationality: str = Field(max_length=20)
     emails: List[EmailStr]
-    friends:  Optional[List[constr(pattern=r'^\d{16}$')]] = None
+
+# Модель для обновления (может быть частичным)
+class UserUpdate(BaseModel):
+    surname: Optional[str]
+    name: Optional[str]
+    patronymic: Optional[str]
+    age: Optional[int]
+    gender: Optional[Literal['m', 'w']]
+    nationality: Optional[str]
+    emails: Optional[List[EmailStr]]
 
 
 app = FastAPI()
 
+def save_to_json(file_path: str, data_dict: dict):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data_dict, f, ensure_ascii=False, indent=4)
 
+@app.put("/users/{user_id}")
+async def update_user(user_id: str, user_update: UserUpdate):
+    if user_id not in data:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    # Обновляем только переданные поля
+    update_data = user_update.dict(exclude_unset=True)
+    data[user_id].update(update_data)
+
+    save_to_json('data.json', data)
+    
+    return data[user_id]
 
 @app.post(                                                                   # endpoint, который принимает информацию о новом человеке и создает запись в БД.  (Пока не в БД)
     "/create_people",
@@ -53,11 +77,14 @@ async def create_people(people: People):
 
 
     data[f"{id:016d}"] = {
-            "name": people.name,
-            "age": people.age,
-            "gender": people.gender,
-            "nationality": people.nationality,
-            "emails": people.emails,
+            "surname" : people.surname,
+            "name" : people.name,
+            "patronymic" : people.patronymic,
+            "age" : people.age,
+            "gender" : people.gender,
+            "nationality" : people.nationality,
+            "emails" : people.emails,
+            "friends" : []
         }
 
     with open('data.json', 'w') as f:
@@ -66,22 +93,35 @@ async def create_people(people: People):
     return {'success':True, 'massege': 'Пользователь успешно добавлен'}
 
 
-@app.get(                                                                   # endpoint, который выводит список всех пользователей.
-    "/get_people{id}",
+@app.get(                                                                   # endpoint, который принимает Фамилию и выводит информацию о пользователе
+    "/get_people{name}",
     tags=['Control and Viewing'],
-    summary='Найти пользователя по id'
+    summary='Найти пользователя по фамилии'
 )
-async def get_people(id: str):
-    return data[id]
+async def get_people(surname: str):
+
+    message = []
+
+    for id in data:
+        if surname == data[id]["surname"]:
+            message.append(data[id])
+            
+    return JSONResponse(message)
 
 
-@app.get(                                                                   # endpoint, принимает id и выводит информацию о пользователе
+@app.get(                                                                   # endpoint, который выводит список всех ФИО.
     "/read_people",
     tags=['Control and Viewing'],
-    summary='Вывести список всех пользователей'
+    summary='Вывести список всех имен пользователей'
 )
 async def read_people():
-    return data
+
+    message = ''
+        
+    for id in data:
+        message += f'{data[id]["surname"]} {data[id]["name"]} {data[id]["patronymic"]}\n'
+
+    return PlainTextResponse(message)
 
 
 ####################################################################################################
